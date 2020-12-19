6022e8d58358bcb0a88ffaf0cd43cc77f24ad737
KennethLG <orsonleiziaga@gmail.com>
2020-12-18 20:56:12 -0500

    xd

--- src/server/routes/apis.ts
+++ src/server/routes/apis.ts
@@ -2,10 +2,10 @@ import express from "express";
 import {VideosService} from "../services/services";
 
 export const videosApi = (app:any, router:any) => {
-	app.use("/api/videos", router);
+	app.use("/api", router);
 	const videosService: any = new VideosService();
 
-	router.get("/", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	router.get("/videos", async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 		try {
 				const videos: any = await videosService.getVideos();
 				res.status(200).json({
@@ -13,6 +13,15 @@ export const videosApi = (app:any, router:any) => {
 					msg: "videos"
 				})
 		} catch (e) {
+			console.error(e);
+			next(e);
+		}
+	});
+
+	router.get("*", async (req, res, next) => {
+		try {
+			res.status(404).send("Not found xd");
+		} catch(e) {
 			console.error(e);
 			next(e);
 		}
